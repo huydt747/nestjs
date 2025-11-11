718c154dd7e95603b4b34b82bb1371181e7b2fd6
Danh Tài <cxinh4295@gmail.com>
2025-11-11 09:53:48 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages/Main.page';
+import AuthPage from './pages/Auth.page';
 
 export const App: React.FC = () => {
 	return (
 		<div>
 			<Routes>
 				<Route path='/' element={<MainPage />} />
-
-				{/* <Route path='/topic/:id' element={<MainPage />} /> */}
-				{/* <Route path='/post/:id' element={<MainPage />} /> */}
-				{/* <Route path='/post/new' element={<MainPage />} /> */}
-
-				{/* <Route path='/login' element={<MainPage />} /> */}
-				{/* <Route path='/register' element={<MainPage />} /> */}
+				<Route path='/auth' element={<AuthPage />} />
 			</Routes>
 		</div>
 	);
