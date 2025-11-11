29d0e9e2657e14e0aeecbf75a09a190c9eb4b409
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-11-11 10:27:45 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage } from './pages/Main.page';
+import MainPage from './pages/Main.page';
+import { Header } from './components';
 
 export const App: React.FC = () => {
 	return (
