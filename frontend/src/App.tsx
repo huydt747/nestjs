97c086f3135a4e0c1da73e409fca4c6921b533f6
huydt747 <103411739+huydt747@users.noreply.github.com>
2025-11-11 09:11:53 +0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,13 @@ export const App: React.FC = () => {
 		<div>
 			<Routes>
 				<Route path='/' element={<MainPage />} />
+
+				{/* <Route path='/topic/:id' element={<MainPage />} /> */}
+				{/* <Route path='/post/:id' element={<MainPage />} /> */}
+				{/* <Route path='/post/new' element={<MainPage />} /> */}
+
+				{/* <Route path='/login' element={<MainPage />} /> */}
+				{/* <Route path='/register' element={<MainPage />} /> */}
 			</Routes>
 		</div>
 	);
