34eea352f1b82b0790dda363260aefff50b2eef0
onhaancom <ihateyoupass@gmail.com>
2025-11-11 10:48:58 +0700

    Update Topic.page.tsx

--- frontend/src/pages/Topic.page.tsx
+++ frontend/src/pages/Topic.page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Pagination from "@/components/Pagination";
+import Pagination from "@/components/pagination";
 import { PostBlock } from "@/components/postblock";
 
 export const TopicPage: React.FC = () => {
